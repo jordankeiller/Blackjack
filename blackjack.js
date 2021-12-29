3ea1f5d4c8486da068523d46fd43c9b80037aaa6
jordankeiller <43140881+jordankeiller@users.noreply.github.com>
2021-12-29 16:03:36 +0000

    Added start game function and function to generate new cards and remove from deck

--- blackjack.js
+++ blackjack.js
@@ -1,18 +1,44 @@
 //attempted 2d array to start with, but wasn't working so switched to a single array and going to parse the string to an integer
-// each card is in one suit. 4 suits * 13 possible values = 52 cards = a deck
+// each card is in one cardSuit. 4 cardSuits * 13 possible cardValues = 52 cards = a deck
 
-let suit = ["Heart", "Diamond", "Club", "Spade"];
-let value = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "K", "Q"];
-let deck = [];
+let cardDeck = [];
+let playerTotal = 0;
 
-for (var i = 0; i < suit.length; i++) {
-  for (var j = 0; j < value.length; j++) {
-    //   console.log(suit[i])
-    //   console.log(value[j])
-    // card[(suit[i], value[j])];
-    deck.push([suit[i],value[j]])
+function newDeck(){
+  let cardSuit = ["Heart", "Diamond", "Club", "Spade"];
+  let cardValue = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "K", "Q"];
+
+  for (var i = 0; i < cardSuit.length; i++) {
+    for (var j = 0; j < cardValue.length; j++) {
+      cardDeck.push([cardSuit[i],cardValue[j]])
+    }
   }
 }
 
-console.log(deck);
-// console.log(suit);
+function generateCard(){
+  // Random item line of code taken from: https://stackoverflow.com/questions/5915096/get-a-random-item-from-a-javascript-array
+  let randomCard = Math.floor(Math.random()*cardDeck.length)
+  let card = cardDeck[randomCard];
+  cardDeck.splice(randomCard,1)
+  return card;
+}
+
+function startingCards(){
+  let cardOne, cardTwo;
+  cardOne = generateCard()
+  cardTwo = generateCard()
+  console.log(cardOne)
+  console.log(cardTwo)  
+}
+
+function startGame(){
+  console.log("===== LOADED =====")
+  newDeck()
+  startingCards()
+
+  
+}
+
+
+
+// console.log(cardDeck);
