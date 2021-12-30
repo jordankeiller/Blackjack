eceadad3489955372e2d0792274f081019667173
jordankeiller <43140881+jordankeiller@users.noreply.github.com>
2021-12-30 22:35:40 +0000

    Added HTML for initialising and updating game state
    HTML to initialise the game, update it when "Hit" button selected and then update when "Stay" button selected.

--- Game.js
+++ Game.js
@@ -8,6 +8,7 @@ class Game {
   }
 
   initialise() {
+    document.getElementById("restart-button").style.visibility = "hidden";
     let newDeck = this.Deck.newDeck();
     for (let i = 0; i < 2; i++) {
       this.drawCard(newDeck);
@@ -18,21 +19,41 @@ class Game {
     let card = this.Deck.pullCard(deck);
     this.Player.addPlayerCards(card);
     this.Player.addPlayerScore(this.Player.getPlayerCardValue(card[1]));
-    this.updateGame();
+    this.updateGame(0);
   }
 
-  updateGame() {
-    document.getElementById("cardDeckTotal").innerHTML =
-      this.Deck.getCardDeckLength();
-    document.getElementById("playerScore").innerHTML =
-      this.Player.getPlayerScore();
-    document.getElementById("playerCards").innerHTML =
-      this.Player.getPlayerCards().join(" ");
-    console.log("game updated!");
+  changeButtonState() {
+    document.getElementById("hit-button").disabled = true;
+    document.getElementById("stay-button").disabled = true;
+    document.getElementById("restart-button").style.visibility = "visible";
+  }
+
+  stay() {
+      console.log("test")
+    this.updateGame(1);
+  }
+
+  updateGame(finished) {
+    if (finished) {
+      this.changeButtonState();
+      document.getElementById("result").innerHTML =
+        "<b>VALID!</b> You have stuck on " + this.Player.getPlayerScore() + "!";
+    } else if (this.Player.getPlayerScore() > 21) {
+      this.changeButtonState();
+      document.getElementById("result").innerHTML = "<b>BUST!</b> You lose!";
+    } 
+      document.getElementById("cardDeckTotal").innerHTML =
+        this.Deck.getCardDeckLength();
+      document.getElementById("playerScore").innerHTML =
+        this.Player.getPlayerScore();
+      document.getElementById("playerCards").innerHTML =
+        this.Player.getPlayerCards().join(" ");
+      console.log("game updated!");
+    
   }
 }
 
-let newGame = new Game();
-newGame.initialise();
+
+
 
 export default Game;
